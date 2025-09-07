90942ae17fb20847143dc57b7f3a8cf8ad4ab2db
Jagadihs Chandra Dhal <jagadishchandra998@gmail.com>
2025-09-07 15:44:07 +0530

    homrpage-react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button';
+
+const Main = () => {
+    return (
+        <>
+            <div className='container'>
+                <div className='p-5 text-center bg-light-dark rounded'>
+                    <h1 className='text-light'>Stock Prediction Portal</h1>
+                    <p className='text-light fs-5 fw-normal'>Welcome to the Stock Prediction Portal - your gateway to smarter investment decisions.
+                        Our platform leverages advanced machine learning models to analyze market trends, track stock performance, and provide accurate predictions.
+                        Stay ahead of the curve and make informed choices with real-time insights at your fingertips.</p>
+                    <Button text='Login' class='btn-outline-info' />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
